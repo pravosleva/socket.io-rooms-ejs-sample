9b83dd1c5239cb76958aed5d51c2affcea9f4145
Filip Troníček <filip.tronicek@seznam.cz>
2020-01-04 18:59:51 +0100

    Remeber username

--- public/script.js
+++ public/script.js
@@ -1,47 +1,77 @@
-const socket = io('http://localhost:3000')
-const messageContainer = document.getElementById('message-container')
-const roomContainer = document.getElementById('room-container')
-const messageForm = document.getElementById('send-container')
-const messageInput = document.getElementById('message-input')
+const socket = io("http://localhost:3000");
+const messageContainer = document.getElementById("message-container");
+const roomContainer = document.getElementById("room-container");
+const messageForm = document.getElementById("send-container");
+const messageInput = document.getElementById("message-input");
 
-if (messageForm != null) {
-  const name = prompt('What is your name?')
-  appendMessage('You joined')
-  socket.emit('new-user', roomName, name)
-
-  messageForm.addEventListener('submit', e => {
-    e.preventDefault()
-    const message = messageInput.value
-    appendMessage(`You: ${message}`)
-    socket.emit('send-chat-message', roomName, message)
-    messageInput.value = ''
-  })
+function setCookie(cname, cvalue, exdays) {
+  var d = new Date();
+  d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
+  var expires = "expires=" + d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
 
-socket.on('room-created', room => {
-  const roomElement = document.createElement('div')
-  roomElement.innerText = room
-  const roomLink = document.createElement('a')
-  roomLink.href = `/${room}`
-  roomLink.innerText = 'join'
-  roomContainer.append(roomElement)
-  roomContainer.append(roomLink)
-})
+function getCookie(cname) {
+  var name = cname + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(";");
+  for (var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == " ") {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
 
-socket.on('chat-message', data => {
-  appendMessage(`${data.name}: ${data.message}`)
-})
+if (messageForm != null) {
+  let name = getCookie("name");
+  if (name == "") {
+    name = prompt("What is your name?");
+    if (name != "" && name != null) {
+      setCookie("name", name, 365);
+    }
+  }
 
-socket.on('user-connected', name => {
-  appendMessage(`${name} connected`)
-})
+  appendMessage("You joined");
+  socket.emit("new-user", roomName, name);
 
-socket.on('user-disconnected', name => {
-  appendMessage(`${name} disconnected`)
-})
+  messageForm.addEventListener("submit", e => {
+    e.preventDefault();
+    const message = messageInput.value;
+    appendMessage(`You: ${message}`);
+    socket.emit("send-chat-message", roomName, message);
+    messageInput.value = "";
+  });
+}
+
+socket.on("room-created", room => {
+  const roomElement = document.createElement("div");
+  roomElement.innerText = room;
+  const roomLink = document.createElement("a");
+  roomLink.href = `/${room}`;
+  roomLink.innerText = "join";
+  roomContainer.append(roomElement);
+  roomContainer.append(roomLink);
+});
+
+socket.on("chat-message", data => {
+  appendMessage(`${data.name}: ${data.message}`);
+});
+
+socket.on("user-connected", name => {
+  appendMessage(`${name} connected`);
+});
+
+socket.on("user-disconnected", name => {
+  appendMessage(`${name} disconnected`);
+});
 
 function appendMessage(message) {
-  const messageElement = document.createElement('div')
-  messageElement.innerText = message
-  messageContainer.append(messageElement)
+  const messageElement = document.createElement("div");
+  messageElement.innerText = message;
+  messageContainer.append(messageElement);
 }
