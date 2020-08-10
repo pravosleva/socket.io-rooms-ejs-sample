d4c7ead31107bd02a1cdeef4425240b53b472d20
Filip Troníček <filip.tronicek@seznam.cz>
2020-08-10 17:22:02 +0000

    Convert function to arrow function

--- server.js
+++ server.js
@@ -52,7 +52,7 @@ io.on('connection', socket => {
   });
 });
 
-function getUserRooms(socket) {
+const getUserRooms = (socket) => {
   return Object.entries(rooms).reduce((names, [name, room]) => {
     if (room.users[socket.id] != null) names.push(name);
     return names;
