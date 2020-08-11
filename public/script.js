a2c5ed407325d6ca13f913a5bafcd6d75aae9c39
Filip Troníček <filip.tronicek@seznam.cz>
2020-08-11 18:44:16 +0200

    ES6 vars

--- public/script.js
+++ public/script.js
@@ -32,9 +32,9 @@ function createUrlFromString(s) {
 }
 
 function setCookie(cname, cvalue, exdays) {
-  var d = new Date();
+  const d = new Date();
   d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
-  var expires = "expires=" + d.toUTCString();
+  const expires = "expires=" + d.toUTCString();
   document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
 
